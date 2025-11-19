e2b18fa63da1478071c8ec54d46fd7ec3bcd4d34
Dumitru-Daniel-Antoniu <ddumitru128@gmail.com>
2025-11-19 16:42:11 +0200

    TASK-1005 - Protection of the gRPC endpoint and asynchronous behavior

--- client/client.py
+++ client/client.py
@@ -1,30 +1,40 @@
+import asyncio
 import grpc
+
+from core.config import GRPC_API_KEY
 
 from weather_pb2 import CityRequest
 from weather_pb2_grpc import WeatherDataStub
 
 
-def main():
+async def main():
     city = input("Enter city name: ")
 
-    channel = grpc.insecure_channel("localhost:50051")
-    client = WeatherDataStub(channel)
-    request = CityRequest(city=city)
+    async with grpc.aio.insecure_channel("localhost:50051") as channel:
+        client = WeatherDataStub(channel)
+        request = CityRequest(city=city)
 
-    try:
-        response = client.Weather(request)
-        print("Weather data received:")
-        print(f"City: {response.city_name}")
-        print(f"Temperature: {response.temperature}°C")
-        print(f"Weather: {response.weather_description}")
-        print(f"Humidity: {response.humidity}%")
-        print(f"Wind Speed: {response.wind_speed}")
-    
-    except grpc.RpcError as e:
-        print("An error occurred:")
-        print(f"Status Code: {e.code()}")
-        print(f"Details: {e.details()}")
+        metadata = [("x-api-key", GRPC_API_KEY)]
+
+        try:
+            response = await client.Weather(request, metadata=metadata)
+            print("Weather data received:")
+            print(f"City: {response.city_name}")
+            print(f"Temperature: {response.temperature}°C")
+            print(f"Weather: {response.weather_description}")
+            print(f"Humidity: {response.humidity}%")
+            print(f"Wind Speed: {response.wind_speed}")
+        
+        except grpc.aio.AioRpcError as e:
+            print("An error occurred:")
+            print(f"Status Code: {e.code()}")
+            print(f"Details: {e.details()}")
+
+        except Exception as e:
+            print("An error occurred:")
+            print(f"Status Code: {e.code()}")
+            print(f"Details: {e.details()}")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
